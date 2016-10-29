1ea655bbd0e98e64dbe0e6a40ffbd8f1951c1a3b
cdzengjiaping <cdzengjiaping@jd.com>
2016-10-29 20:27:04 +0800

    fix bug

--- src/main/java/org/smart4j/framework/HelperLoader.java
+++ src/main/java/org/smart4j/framework/HelperLoader.java
@@ -3,6 +3,7 @@ package org.smart4j.framework;
 import org.smart4j.framework.annotation.Controller;
 import org.smart4j.framework.helper.BeanHelper;
 import org.smart4j.framework.helper.ClassHelper;
+import org.smart4j.framework.helper.ControllerHelper;
 import org.smart4j.framework.helper.IocHelper;
 import org.smart4j.framework.util.ClassUtil;
 
@@ -21,7 +22,7 @@ public class HelperLoader {
                 ClassHelper.class,
                 BeanHelper.class,
                 IocHelper.class,
-                Controller.class
+                ControllerHelper.class
         };
         for (Class<?> cls : classList) {
             ClassUtil.loadClass(cls.getName(), true);
